ee2fdd59905ba2880b5c2544b335fa77889f2696
Gabor Szabo <gabor@szabgab.com>
2024-06-21 15:03:08 +0300

    add multiline comment

--- examples/single-line-comments/comments.c
+++ examples/single-line-comments/comments.c
@@ -3,9 +3,18 @@
 // This is a comment
 
 int main() {
-    printf("Hello World!\n");  // This is a comment too
+    printf("Hello ");  // This is a comment too
 
     // We don't want the following line of code to be included:
     // printf("Hello World!\n");
+
+    printf("World");
+
+    /*
+      A multi-line comment
+      With a beginning and an end.
+     */
+
+    printf("!\n");
 }
 
